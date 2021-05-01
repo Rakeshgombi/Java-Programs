c437e2c2bda24530f7cd5c2ad64355b91cfbd751
Rakesh Gombi <rakeshgombi.ec19@rvce.edu.in>
2021-05-01 16:25:29 +0530

    Update Exercise8.java

--- Exercise8.java
+++ Exercise8.java
@@ -22,7 +22,7 @@ import java.util.Scanner;
 public class Program
 {
     public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	Scanner sc = new Scanner(System.in);
         System.out.println("Input Number of Inputs: ");
         int n = sc.nextInt();
         int sum = 0, a;
